f9636e897e2e9ceb12950d8a7210a36919ac5a89
RosanaAlves <rosanaalvesmolina7@gmail.com>
2023-02-14 11:57:32 -0300

    update 1

--- Recursos Humanos/R/Analise descritiva.R
+++ Recursos Humanos/R/Analise descritiva.R
@@ -5,21 +5,68 @@
 require(dplyr)
 require(readr)
 require(ggplot2)
+library(tidyverse)
+library(caret)
 
 dados <- read.csv("Data/WA_Fn-UseC_-HR-Employee-Attrition.csv")
-
 
 # -------------------------------------------------------------------------
 
 
 # Analise exploratoria ----------------------------------------------------
 
-# variavel de interesse é Atrition (saída do funcionário)
+## variavel de interesse é Atrition (saída do funcionário)
 
-table(dados$Attrition)
+summary(dados)
 
+Dep <- table(dados$Department)
+# pairs(dados)
 
+attritionY <- dados%>%
+  filter(Attrition == "Yes")
+DepY <- table(attritionY$Department)
+
+## Proporção de sáidas de funcionários por departamento
+
+SpD <- c()
+for(i in 1:3){
+  SpD[i] = (DepY[i]/Dep[i])*100
+}
+
+## O departamento que tem mais pedidos de demissão é o de vendas (Sales), seguido pelo recursos humanos e por fim pesquisa e desenvolvimento.
 
 # -------------------------------------------------------------------------
 
 
+# Visualizações -----------------------------------------------------------
+# library(DataEditR)
+# library(fastDummies) ## data_edit(dados)
+
+AttritionDummy <- c()
+for(i in 1:length(dados$Attrition)){
+  if(dados$Attrition[i] == "Yes"){
+    AttritionDummy[i] = 1
+  } else {AttritionDummy[i] = 0}
+}
+table(AttritionDummy)
+table(dados$Attrition)
+
+dados$Attrition <- AttritionDummy
+
+#Proporçoes
+
+prop.table(table(dados$Attrition,dados$Gender)) #Tabela cruzada, Da a proporçao de cada variavel em cada categoria em relaçao ao total
+AttGen <- table(dados$Attrition,dados$Gender)
+propG <- c()#Proporção de pedido de demissão por genero
+for(i in 1:2){
+  propG[i] =  AttGen[]
+}
+
+ggplot(dados) +
+  geom_bar( aes(x=Gender, y=Attrition), stat="identity", fill="skyblue", alpha=0.7)+
+  labs(title = "Pedidos de demissão por Gênero")
+
+
+
+ggplot(dados) +
+  geom_bar( aes(x=EducationField, y=Attrition), stat="identity", fill="skyblue", alpha=0.7)
